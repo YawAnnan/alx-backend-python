45adda3541303991256694df7b512ed2c2f15c61
YawAnnan <annanamoahisaac585@outlook.com>
2025-09-21 01:13:13 +0000

    Add Django project with DRF and chats app

---
+++ messaging_app/messaging_app/settings.py
@@ -0,0 +1,25 @@
+INSTALLED_APPS = [
+    # Django default apps...
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # Third-party apps
+    'rest_framework',
+
+    # Your apps
+    'chats',
+]
+
+# Django REST Framework global settings
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+}
