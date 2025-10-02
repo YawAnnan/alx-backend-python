75b06f88aa0eb43c949860a3723839a2758b309f
YawAnnan <annanamoahisaac585@outlook.com>
2025-10-02 11:00:41 +0000

    Implement signals for notifications and message edit history

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import User
 
 
@@ -36,6 +37,8 @@ class MessageHistory(models.Model):
     message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name="history")
     old_content = models.TextField()
     edited_at = models.DateTimeField(auto_now_add=True)
-
+    edited_by = models.ForeignKey(   # ✅ NEW
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True
+    )
     def _str_(self):
         return f"History of message {self.message.id} at {self.edited_at}"
